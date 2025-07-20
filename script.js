502f97a36891705fa9341e6aa7a962b08b021b85
Yanarkaev <hasssambek@gmail.com>
2025-07-20 12:29:28 +0300

    расписаны задачи

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
 let key = 'db69ca8c35f1409f80391800250507'
 let api = `http://api.weatherapi.com/v1/current.json?key=${key}&q=Урус-Мартан&lang=ru`
 
+let object = {
+    "location": {
+        "name": "Урус-Мартан",
+        "region": "Chechnya Republic",
+        "country": "Россия",
+        "lat": 43.1286,
+        "lon": 45.5431,
+        "tz_id": "Europe/Moscow",
+        "localtime_epoch": 1753003398,
+        "localtime": "2025-07-20 12:23"
+    },
+    "current": {
+        "last_updated_epoch": 1753002900,
+        "last_updated": "2025-07-20 12:15",
+        "temp_c": 37.1,
+        "temp_f": 98.7,
+        "is_day": 1,
+        "condition": {
+            "text": "Солнечно",
+            "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
+            "code": 1000
+        },
+        "wind_mph": 5.4,
+        "wind_kph": 8.6,
+        "wind_degree": 350,
+        "wind_dir": "N",
+        "pressure_mb": 1004,
+        "pressure_in": 29.64,
+        "precip_mm": 0,
+        "precip_in": 0,
+        "humidity": 27,
+        "cloud": 5,
+        "feelslike_c": 37.6,
+        "feelslike_f": 99.7,
+        "windchill_c": 37.1,
+        "windchill_f": 98.7,
+        "heatindex_c": 37.6,
+        "heatindex_f": 99.7,
+        "dewpoint_c": 14.8,
+        "dewpoint_f": 58.7,
+        "vis_km": 10,
+        "vis_miles": 6,
+        "uv": 8.9,
+        "gust_mph": 6.2,
+        "gust_kph": 9.9
+    }
+}
 
-async function getWeather (){
+async function getWeather() {
     let request = await fetch(api);
     let response = await request.json();
     console.log(response)
 }
 
 getWeather()
+
+
+// Задача 1. Получить инпут и кнопку. по клику на кнопку выводить текст из инпута в консоль | Айша
+// Задача 2. Написать функцию, которая отображает данные из объекта на страницу в соответствующие поля. Нужно получить элементы из HTML и вставлять в них текст из объекта с помощью innerHTML | Камила
