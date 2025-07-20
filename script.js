969b50baf6c9ea72c2c4b13aa33563a346844bee
Yanarkaev <hasssambek@gmail.com>
2025-07-20 12:18:53 +0300

    базовый апи йина вай доьхьн

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const key = 'db69ca8c35f1409f80391800250507'
-const api = `http://api.weatherapi.com/v1/current.xml?key=${key}&q=Урус-Мартан`
+let key = 'db69ca8c35f1409f80391800250507'
+let api = `http://api.weatherapi.com/v1/current.json?key=${key}&q=Урус-Мартан&lang=ru`
+
+
+async function getWeather (){
+    let request = await fetch(api);
+    let response = await request.json();
+    console.log(response)
+}
+
+getWeather()
